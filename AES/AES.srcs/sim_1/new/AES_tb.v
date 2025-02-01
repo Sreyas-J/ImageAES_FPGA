d1957a282c9ac090e78413482d4a55925f1cce2e
Sreyas-J <Sreyas.Janamanchi@iiitb.ac.in>
2025-02-01 14:45:23 +0530

    pipelining almost done

--- AES/AES.srcs/sim_1/new/AES_tb.v
+++ AES/AES.srcs/sim_1/new/AES_tb.v
@@ -4,17 +4,17 @@ module AES_tb;
 
     // Testbench variables
     reg clk;
-    wire [127:0] in=128'hFFD8FFE000104A464946000101000001;
+    wire [127:0] in;
     reg rst;
     wire [127:0] encrypted128;
 
     // Instantiate the AES module
     AES dut (
         .clk(clk),              // Connect clock
-        .in(in),
         .rst(rst),
-        .encrypted128(encrypted128), // Connect encrypted128
-        .done(done)
+        .in(in),
+        .encrypted128(encrypted128) // Connect encrypted128
+//        .done(done)
     );
 
     // Clock generation
@@ -25,10 +25,13 @@ module AES_tb;
     // Initial block for stimulus
     initial begin
         clk = 0;
+//        in=128'hFFD8FFE000104A464946000101000001;
         rst=1;
         #20
         rst=0;
-        #20000
+//        #900
+//        in=128'h00010000FFDB00430008060607060508;
+        #900
         $finish;
     end
 
--- AES/AES.srcs/sources_1/imports/new/AES_Encryption.v
+++ AES/AES.srcs/sources_1/imports/new/AES_Encryption.v
@@ -5,10 +5,10 @@ module AES_Encrypt(
     input [(128*11)-1:0] fullkeys,
     output reg [127:0] out,
     output reg done
-//    output wire [(128*11)-1:0] states_out
 );
 
     reg [127:0] addrkIn;
+    reg [7:0] rnd;
     wire [127:0] addrkOut;
     reg [127:0] sIn;
     wire [127:0] sOut;
@@ -17,13 +17,9 @@ module AES_Encrypt(
     reg [127:0] mIn;
     wire [127:0] mOut;
     
+    reg [127:0] pipR;    
     reg [5:0] cntr;
     reg [127:0] key;
-//    reg [127:0] states [11:0];
-//    wire [127:0] buff;
-//    wire [127:0] afterSubBytes;
-//    wire [127:0] afterShiftRows;
-//    wire [127:0] encryptOut [10:0]; // Wires for storing intermediate encryptRound outputs
 
     addRoundKey addrk1 (addrkIn, addrkOut, key);
     subBytes s(sIn,sOut);
@@ -32,60 +28,98 @@ module AES_Encrypt(
     
     always@(posedge clk) begin
         if(rst) cntr<=4'd0;
-        else if(cntr==6'd40) begin
-//            cntr<=4'd0;
+        else if(cntr==6'd44) begin
             done<=1'b1;
+            
             out<=addrkOut;
-        end
-        else begin
-            done<=1'b0;
-            key<=fullkeys[(((128*11)-1)-128*((cntr+1)/3'd4))-:128];
-         
-            if(cntr==6'd0)begin
-              addrkIn<=in;
-            end
-            else if(cntr==6'd39) addrkIn<=rOut;
-
-            else if(cntr%3'd4==2'd1) sIn<=addrkOut;
-            else if(cntr%3'd4==2'd2) rIn<=sOut;
-            else if(cntr%3'd4==2'd3) mIn<=rOut;
-            else if(cntr%3'd4==2'd0) addrkIn<=mOut;
             
             cntr<=cntr+1;
         end
-        $display("counter:%d sOut:%h rOut:%h mixColOut:%h addrkOut:%h",cntr,sOut,rOut,mOut,addrkOut);
-    end
+        else if(cntr==6'd40) begin
+            done<=1'b1;
+            out<=addrkOut;
+            
+            pipR<=rOut;
+            addrkIn<=mOut;
+            rIn<=sOut;
+            
+            cntr<=cntr+1;
+        end
+        else if(cntr==6'd42) begin
+            done<=1'b1;
+            
+            out<=addrkOut;
+            addrkIn<=rOut;
+            rIn<=sOut;
+            
+            cntr<=cntr+1;
+        end
+        else if(cntr==6'd43) begin
+            done<=1'b1;
+            
+            out<=addrkOut;
+            addrkIn<=rOut;
+            
+            cntr<=cntr+1;
+        end
+        else begin
+            done<=1'b0;
+         
+            if(cntr==6'd0)begin
+//                key<=fullkeys[(((128*11)-1))-:128];
+                addrkIn<=in;
+            end
+            
+            else if(cntr==6'd1) begin
+                addrkIn<=in;
+                
+                sIn<=addrkOut;
+            end
+            else if(cntr==6'd2) begin
+                addrkIn<=in;
+                
+                rIn<=sOut;
+                sIn<=addrkOut;
+            end
+            else if(cntr==6'd3) begin
+                addrkIn<=in;
+                
+                mIn<=rOut;
+                rIn<=sOut;
+                sIn<=addrkOut;
+            end
+            else if(cntr==6'd39) begin
+                key<=fullkeys[(((128*11)-1)-128*10)-:128];
+                addrkIn<=rOut;
+                
+                rIn<=sOut;                
+                sIn<=addrkOut;
+            end
+            
+            else if(cntr==6'd41) begin
+                addrkIn<=pipR;
+                sIn<=addrkOut;
+            end
+            
+            else begin
+//                key<=fullkeys[(((128*11)-1)-128*((cntr+1)/3'd4))-:128];
+                             
+                addrkIn<=mOut;
+                mIn<=rOut;
+                rIn<=sOut;
+                sIn<=addrkOut;
+            end
+           
+          if(cntr%3'd4==2'd1) key<=fullkeys[(((128*11)-1)-128*((cntr-1)/3'd4))-:128];
+          else if(cntr%3'd4==2'd2) key<=fullkeys[(((128*11)-1)-128*((cntr-2)/3'd4))-:128];
+          else if(cntr%3'd4==2'd3) key<=fullkeys[(((128*11)-1)-128*((cntr+1)/3'd4))-:128];
+          else if(cntr%3'd4==2'd0)  key<=fullkeys[(((128*11)-1)-128*((cntr)/3'd4))-:128];
 
-//    // Generate encryption rounds
-//    genvar i;
-//    generate
-//        for (i = 1; i < 10; i = i + 1) begin : loop
-//            encryptRound er (
-//                .in(states[i-1]),
-//                .key(fullkeys[(((128*11)-1)-128*i)-:128]),
-//                .out(encryptOut[i])
-//            );
-//        end
-//    endgenerate
-
-//    integer j;
-//    always @(posedge clk) begin
-//        // Update states for each round
-//        for (j = 9; j >= 0; j = j - 1) begin
-//            states[j] <= encryptOut[j];
-//        end
+                       
+            cntr<=cntr+1;
+        end
         
-//    end
-    
-////    generate
-////        for (i = 0; i < 11; i = i + 1) begin : flatten_states
-////            assign states_out[(i*128)+:128] = states[i];
-////        end
-////    endgenerate
-
-//    // Final Round Logic - instantiated outside of always block
-//    subBytes sb (states[9], afterSubBytes);
-//    shiftRows sr (afterSubBytes, afterShiftRows);
-//    addRoundKey addrk2 (afterShiftRows, out, fullkeys[127:0]);
+        $display("key:%h cntr:%d done:%d in:%h addrkIn:%h addrkOut:%h sIn:%h sOut:%h rIn:%h rOut:%h mixColIn:%h mixColOut:%h", key, cntr, done,in, addrkIn, addrkOut, sIn, sOut, rIn, rOut, mIn, mOut);
+    end
 
 endmodule
